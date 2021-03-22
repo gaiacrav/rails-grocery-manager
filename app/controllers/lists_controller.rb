add26cc5c6715da2e2e31dcddb70162542c3d480
Gaia Cravesana <gcravesana@gmail.com>
2021-03-22 17:14:58 +0000

    created the new page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,6 +2,7 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.all
+    @ingredients = Ingredient.all
   end
 
   def show
@@ -9,12 +10,25 @@ class ListsController < ApplicationController
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.create(list_params)
+    if @list.save!
+      redirect_to lists_path
+    else
+      render :new
+    end
   end
 
   def delete
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :ingredient, :completed)
   end
 
 end
